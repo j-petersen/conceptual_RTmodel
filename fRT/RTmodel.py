fixed a bug in the the calculation with stokes
changed the 1 in the scattering term to the unity matrix
added additional cases in the angle transformation
might be a bug in the direction?

fRT/RTmodel.py
@@ -284,9 +284,8 @@ class RT_model_1D(object):
         idx, height = f.argclosest(height, self.height_array, return_value=True)
         angle = (self.sun_elevation + 90) % 180
 
-        tau = np.zeros((self.stokes_dim,self.stokes_dim))
+        tau = np.zeros((self.stokes_dim, self.stokes_dim))
         for lvl in np.arange(len(self.height_array) - 1, idx - 1, -1):
-
             tau += (
                 (
                     self.absorption_coeff_field[lvl]
@@ -315,11 +314,11 @@ class RT_model_1D(object):
         # setting the phase function according to the selected scattering type
         if self.scat_type:
             phase_func = f.transformed_rayleigh_scattering_matrix(
-            self.sun_elevation,
-            self.receiver_elevation,
-            self.sun_azimuth,
-            self.receiver_azimuth,
-            self.stokes_dim
+                self.sun_elevation,
+                self.receiver_elevation,
+                self.sun_azimuth,
+                self.receiver_azimuth,
+                self.stokes_dim,
             )
             # phase_func = f.rayleigh_phasematrix(angle, self.stokes_dim)
         else:
@@ -333,7 +332,8 @@ class RT_model_1D(object):
             )
         else:
             I_scat = (
-                (1 - expm(-self.scattering_coeff_field[idx] * self.swiping_height))
+                (np.eye(self.stokes_dim) - expm(-self.scattering_coeff_field[idx] *
+                                                self.swiping_height))
                 @ RT_model_1D.calc_direct_beam_intensity(self, height)
                 @ phase_func
             )
@@ -444,11 +444,7 @@ class RT_model_1D(object):
             scat = RT_model_1D.scattering_source_term(self, height)
             planck = RT_model_1D.planck_source_term(self, height)
             # id starts at 0 for idx 1 from height at rad field!
-            rad_field[id + 1] = (
-                ext
-                + self.use_scat * scat
-                + self.use_planck * planck
-            )
+            rad_field[id + 1] = ext + self.use_scat * scat + self.use_planck * planck
 
         # invert the rad_field for the uplooking case
         return np.flipud(rad_field)

fRT/functions/transformations.py
@@ -34,18 +34,18 @@ def transformation_angle(theta_in, theta_out, phi_in, phi_out):
             np.cos(np.deg2rad(theta_out))
             - np.cos(np.deg2rad(theta_in)) * np.cos(np.deg2rad(theta_sca))
         )
-        / np.sin(np.deg2rad(theta_in))
-        * np.sin(np.deg2rad(theta_sca))
+        / (np.sin(np.deg2rad(theta_in))
+        * np.sin(np.deg2rad(theta_sca)))
     )
     sigma2 = np.arccos(
         (
             np.cos(np.deg2rad(theta_in))
             - np.cos(np.deg2rad(theta_out)) * np.cos(np.deg2rad(theta_sca))
         )
-        / np.sin(np.deg2rad(theta_out))
-        * np.sin(np.deg2rad(theta_sca))
+        / (np.sin(np.deg2rad(theta_out))
+        * np.sin(np.deg2rad(theta_sca)))
     )
-    return sigma1, sigma2
+    return np.rad2deg(sigma1), np.rad2deg(sigma2)
 
 
 def transformed_rayleigh_scattering_matrix(
@@ -64,12 +64,56 @@ def transformed_rayleigh_scattering_matrix(
     if phi_out < 0 and phi_out >= 360:
         raise ValueError("Phi out cannot be negative or >= 360")
 
-    theta_sca = calc_scattering_angle(theta_in, theta_out, phi_in, phi_out)
-    sigma1, sigma2 = transformation_angle(theta_in, theta_out, phi_in, phi_out)
 
-    L1 = stokes_rotation_matrix(-sigma2)
-    L2 = stokes_rotation_matrix(180 - sigma1)
+    theta_sca = calc_scattering_angle(theta_in, theta_out, phi_in, phi_out)
     F = rayleigh_phasematrix(theta_sca)
 
-    P = L1 @ F @ L2
+    P = np.zeros((stokes_dim,stokes_dim))
+
+    # for stokes dim = 1, we only need Z11 = F11
+    P[0,0] = F[0, 0]
+    if stokes_dim == 1:
+        return P.squeeze()
+
+    #
+    # Multiple cases have to be considered:
+    #
+    ANGTOL_RAD = 1e-6 # absolut tolerenz for angle
+    ANGTOL = np.rad2deg(ANGTOL_RAD)
+
+    if (
+            abs(theta_sca) < ANGTOL             # forward scattering
+            or abs(theta_sca - 180) < ANGTOL   # backward scattering
+            or abs(phi_in - phi_out) < ANGTOL  # inc and sca on meridian
+            or abs(abs(phi_in - phi_out) - 360.) < ANGTOL
+            or abs(abs(phi_in - phi_out) - 180.) < ANGTOL):
+
+        P[0,1], P[1,0], P[1,1] = F[0,1], F[1,0], F[1,1]
+        if stokes_dim > 2:
+            P[2,2] = F[2,2]
+            # other elements are 0
+            if stokes_dim > 3:
+                P[2,3], P[3,2], P[3,3] = F[2,3], F[3,2], F[3,3]
+                # other elements are 0
+    else:
+        if theta_in < ANGTOL:
+            sigma1 = 180 + phi_out - phi_in
+            sigma2 = 0
+        elif theta_in > 180 - ANGTOL:
+            sigma1 = phi_out - phi_in
+            sigma2 = 180
+        elif theta_out < ANGTOL:
+            sigma1 = 0
+            sigma2 = 180 + phi_out - phi_in
+        elif theta_out > 180 - ANGTOL:
+            sigma1 = 180
+            sigma2 = phi_out - phi_in
+        else:
+            sigma1, sigma2 = transformation_angle(theta_in, theta_out, phi_in, phi_out)
+
+        L1 = stokes_rotation_matrix(180 - sigma1)
+        L2 = stokes_rotation_matrix(-sigma2)
+
+        P = L2 @ F @ L1
+
     return P[:stokes_dim, :stokes_dim]

main.py
@@ -54,25 +54,34 @@ def test(rt):
     # fig.savefig('plots/height_scattering_source.png', dpi=300)
 
 
-def plotting_radiation_height(rt):
-    rad_field = rt.evaluate_radiation_field()[:,1]
+def plotting_radiation_height(rt, stokes_dim = None):
+    rad_field = rt.evaluate_radiation_field()[:,:]
     height_field = rt.get_receiver_viewing_field()
 
     max_height = 200
     height_level = None
+    if not stokes_dim:
+        stokes_dim = np.shape(rad_field)[1]
 
+    labels = ['I', 'Q', 'U', 'V']
     fig, ax = plt.subplots()
-    ax.plot(rad_field, f.m2km(height_field))
+    for i in range(stokes_dim):
+        ax.plot(rad_field[:,i], f.m2km(height_field), label=labels[i])
     if height_level is not None:
         ax.axhline(height_level, alpha=0.7)
 
     if max_height is not None:
         ax.set_ylim(0, max_height)
 
-    # ax.set_xlabel(r"Intensity / a.u.")
+    ax.set_title(rf"Receiver: h={rt.receiver_height}, " + \
+                rf"$\Theta$={(rt.receiver_elevation + 90) % 180}, " + \
+                rf"$\phi$={(rt.receiver_azimuth + 180) % 360}" + '\n'\
+                rf"Sun: $\Theta$={(rt.sun_elevation + 90) % 180}, " + \
+                rf"$\phi$={(rt.sun_azimuth + 180) % 360}",
+                fontdict = {'fontsize':12}, va='bottom')
     ax.set_xlabel(r"Intensity / $Wm^{-2}sr^{-1}m^{-1}$")
     ax.set_ylabel("height / km")
-
+    ax.legend()
     # fig.savefig('plots/height_intensity_from_ground', dpi=300)
 
 
@@ -123,6 +132,20 @@ def plot_phasefunction():
     #             fontdict = {'fontsize':16}, va='bottom')
     # fig.savefig('plots/rayleigh_phasefunc.png', dpi=300)
 
+
+def plot_deg_of_polarization(rt, receiver):
+    angles = np.linspace(0, 90, 19, endpoint=True)
+    receiver_field = np.empty((len(angles),4))
+    for id, angle in enumerate(angles):
+        rt.set_receiver(receiver.height, receiver.elevation, receiver.azimuth+angle)
+        receiver_field[id] = rt.evaluate_radiation_field()[0]
+
+    p = f.degree_of_polarization(receiver_field)
+    fig, ax = plt.subplots()
+    ax.plot(angles, p)
+    ax.set_xlabel(r"azimuth angle diff / deg")
+    ax.set_ylabel("degree of polarization")
+    fig.savefig('plots/deg_of_polarization', dpi=300)
 
 def plot_blue_red_sky(rt, sun, receiver, scaled=False):
     wavelenghs = np.array((400, 550, 700)) * 1e-9
@@ -213,13 +236,14 @@ def plot_sky_stationary_sun(rt, sun, receiver):
 if __name__ == "__main__":
     ty.plots.styles.use(["typhon", "typhon-dark"])
 
-    receiver = fRT.Receiver(height=0, ele=45, azi=180)
-    sun = fRT.Sun(ele=45, azi=180)
+    receiver = fRT.Receiver(height=0, ele=80, azi=0)
+    sun = fRT.Sun(ele=80, azi=90)
 
     model = model_setup(sun, receiver)
     # test(model)
     plotting_radiation_height(model)
     # plotting_radiation_at_viewingangle(model, sun, receiver)
+    # plot_deg_of_polarization(model, receiver)
     # plot_blue_red_sky(model, sun, receiver, scaled=True)
     # plot_phasefunction()
 