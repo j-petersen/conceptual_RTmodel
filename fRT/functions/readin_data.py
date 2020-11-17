771964571a64fdda6cffd757833e1cc27b99e294
Jon <jon.petersen@web.de>
2020-11-17 11:57:07 +0100

    added data files and fitting readin methodes
    Everyone can now run the model themself!

--- fRT/functions/readin_data.py
+++ fRT/functions/readin_data.py
@@ -1,32 +1,22 @@
 import numpy as np
+from pathlib import Path
 
 __all__ = ["readin_densprofile", "readin_tempprofile"]
 
-
 def readin_densprofile():
-    PATH = "/Users/jonpetersen/data/data_BA/"
-    MSIS_DATEI = "MSIS/MSIS_18072300_new.txt"
-    with open(PATH + MSIS_DATEI) as msis:
-        MSISdata = np.genfromtxt(msis, skip_header=11)
-    # 0 Height, km | 1 O, cm-3 | 2 N2, cm-3 | 3 O2, cm-3 |
-    # 4 Mass_density, g/cm-3 | 5 Ar, cm-3
-    MSISalt = MSISdata[:, 0]  # Altitude
-    MSISdens = (
-        MSISdata[:, 1] + MSISdata[:, 2] + MSISdata[:, 3] + MSISdata[:, 4]
-    ) * 10 ** 6  # add desitys and convert to SI units
-    return MSISalt, MSISdens
+    file = Path("fRT/data/density.txt")
+    with open(file, 'r') as file:
+        data = np.genfromtxt(file, skip_header=6, delimiter=';')
 
+    altitude = data[:, 1]  # Altitude
+    density = (data[:, 2]) * 10 ** 6  # convert to SI units
+    return altitude, density
 
 def readin_tempprofile():
-    PATH = "/Users/jonpetersen/data/data_BA/"
-    FILE = "T_Fit_54N_Tab.txt"
-    with open(PATH + FILE) as f:
-        data = np.genfromtxt(f, skip_header=11, usecols=None)
+    file = Path("fRT/data/temperature.txt")
+    with open(file, 'r') as file:
+        data = np.genfromtxt(file, skip_header=6, delimiter=';')
 
-    altitude = np.empty((len(data)))
-    mean_temp = np.empty((len(data)))
-    for id, row in enumerate(data):
-        altitude[id] = row[0]
-        mean_temp[id] = np.mean(row[1:])
-
-    return altitude, mean_temp
+    altitude = data[:, 1]  # Altitude
+    temperature = (data[:, 2])
+    return altitude, temperature
